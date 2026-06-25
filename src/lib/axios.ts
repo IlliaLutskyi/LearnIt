e5aa87881588a3f01f81f29e479f75f21935402d
IlliaLutskyi <axax09787@gmail.com>
2026-06-25 16:25:53 +0300

    fixed axios baseUrl again 2

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : "http://localhost:3000"}/api`,
 });
 
 export default api;
